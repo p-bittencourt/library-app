ca3a675caa3b54a44d18b91bde93bcd57532a01f
Pedro Bittencourt <pedromonteirobittencourt@gmail.com>
2024-03-07 12:15:53 -0300

    Validated form submition and Book construction

--- src/components/BookRegister.jsx
+++ src/components/BookRegister.jsx
@@ -8,11 +8,26 @@ const BookRegister = () => {
   const [read, setRead] = useState(false);
   const [info, setInfo] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let pagesValue = parseInt(pages);
+    let infoValue = info;
+
+    if (isNaN(pagesValue)) {
+      pagesValue = 0;
+    } else if (infoValue === "") {
+      infoValue = "Book description not informed.";
+    }
+
+    const newBook = new Book(title, author, pagesValue, infoValue, read);
+  };
+
   return (
     <>
       <div className="container">
         <h2 className="mt-3">Book Register</h2>
-        <form action="" className="mt-3 p-3">
+        <form onSubmit={handleSubmit} className="mt-3 p-3">
           <div className="mb-3">
             <label htmlFor="bookTitle" className="form-label">
               Book Title
@@ -22,6 +37,8 @@ const BookRegister = () => {
               className="form-control"
               id="bookTitle"
               placeholder="The Hobbit"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
               required
             />
           </div>
@@ -34,6 +51,8 @@ const BookRegister = () => {
               className="form-control"
               id="bookAuthor"
               placeholder="JRR Tolkien"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
               required
             />
           </div>
@@ -47,6 +66,10 @@ const BookRegister = () => {
                 className="form-control"
                 id="bookPages"
                 placeholder="310"
+                value={pages}
+                onChange={(e) => {
+                  setPages(e.target.value);
+                }}
               />
             </div>
           </div>
@@ -59,6 +82,8 @@ const BookRegister = () => {
                   className="form-check-input"
                   id="isRead"
                   name="readStatus"
+                  checked={read}
+                  onChange={() => setRead(true)}
                 />
                 <label htmlFor="isRead" className="form-check-label">
                   I've read it.
@@ -70,7 +95,8 @@ const BookRegister = () => {
                   className="form-check-input"
                   id="isNotRead"
                   name="readStatus"
-                  defaultChecked
+                  checked={!read}
+                  onChange={() => setRead(false)}
                 />
                 <label htmlFor="isNotRead" className="form-check-label">
                   I haven't read it yet.
@@ -88,6 +114,8 @@ const BookRegister = () => {
               rows="5"
               className="form-control"
               placeholder="The Hobbit, or There and Back Again is a children's fantasy novel by the English author J. R. R. Tolkien. It was published in 1937 to wide critical acclaim."
+              value={info}
+              onChange={(e) => setInfo(e.target.value)}
             ></textarea>
           </div>
           <div className="button-container text-end">
