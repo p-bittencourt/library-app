af9f1bef44e1fb8a20b80dad3c3c1e1469dba8bb
Pedro Bittencourt <pedromonteirobittencourt@gmail.com>
2024-03-07 13:13:26 -0300

    began implementing ShowLibrary component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import BookRegister from "./components/BookRegister";
 import "./styles/css/styles.css";
+import ShowLibrary from "./components/ShowLibrary";
 
 function App() {
+  const [showRegister, setShowRegister] = useState(true);
+
   return (
     <>
       <header>
@@ -9,7 +13,17 @@ function App() {
           <h1 className="main-title">My Library</h1>
         </div>
       </header>
-      <BookRegister />
+      <div className="container">
+        <div className="button-container text-center">
+          <button
+            className="btn btn-secondary m-2"
+            onClick={() => setShowRegister(!showRegister)}
+          >
+            {showRegister ? "Show Library" : "Register New Book"}
+          </button>
+        </div>
+        {showRegister ? <BookRegister /> : <ShowLibrary />}
+      </div>
     </>
   );
 }
---
+++ src/components/ShowLibrary.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShowLibrary = () => {
+  return <div>ShowLibrary</div>;
+};
+
+export default ShowLibrary;
